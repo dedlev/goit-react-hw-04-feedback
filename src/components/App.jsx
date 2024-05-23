436dd3a754f5352eb28a77418aa934098af353a8
dedlev <l.popchuk@gmail.com>
2024-05-23 13:40:38 +0300

    adds a step 1
    adds a step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,58 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  handleIncrementGood = () => {
+    this.setState(prevState => {
+      return { good: prevState.good + 1 };
+    });
+  };
+
+  handleIncrementNeutral = () => {
+    this.setState(prevState => {
+      return { neutral: prevState.neutral + 1 };
+    });
+  };
+
+  handleIncrementBad = () => {
+    this.setState(prevState => {
+      return { bad: prevState.bad + 1 };
+    });
+  };
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div
+        style={{
+          height: '100vh',
+          padding: '40px',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h2>Please leave feedback</h2>
+        <div>
+          <button type="button" onClick={this.handleIncrementGood}>
+            Good
+          </button>
+          <button type="button" onClick={this.handleIncrementNeutral}>
+            Neutral
+          </button>
+          <button type="button" onClick={this.handleIncrementBad}>
+            Bad
+          </button>
+        </div>
+
+        <h2>Statistics</h2>
+        <p>Good:{good}</p>
+        <p>Neutral:{neutral}</p>
+        <p>Bad:{bad}</p>
+      </div>
+    );
+  }
+}
